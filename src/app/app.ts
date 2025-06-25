f55543d4e1615c8469971506a73f76a0467636b2
maksimkasyanov <maksim.kasyanov.1995@gmail.com>
2025-06-25 18:47:48 +0300

    first push

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './structure/header/header';
+import {Main} from './structure/main/main';
+import {Footer} from './structure/footer/footer';
+import {NoiseBackgroundDirective} from './directives/noize/noise-background';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
+  imports: [
+    Header,
+    Main,
+    Footer,
+    NoiseBackgroundDirective
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/directives/noize/noise-background.spec.ts
@@ -0,0 +1,8 @@
+import { NoiseBackground } from './noise-background';
+
+describe('NoiseBackground', () => {
+  it('should create an instance', () => {
+    const directive = new NoiseBackground();
+    expect(directive).toBeTruthy();
+  });
+});
